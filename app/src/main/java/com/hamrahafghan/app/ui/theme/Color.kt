7ff3f4dfa8d02d49e9ca8a9454a377f3b6e6785f
Hamrah Afghan <hamrah-afghan@users.noreply.github.com>
2026-09-11 17:03:37 +0330

    Add app theme colors

---
+++ app/src/main/java/com/hamrahafghan/app/ui/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.hamrahafghan.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val HamrahPrimary = Color(0xFF013CA8)
+val HamrahOnPrimary = Color(0xFFFFFFFF)
+val HamrahPrimaryContainer = Color(0xFFDCE1FF)
+val HamrahOnPrimaryContainer = Color(0xFF001453)
+
+val HamrahSecondary = Color(0xFF5B5D72)
+val HamrahOnSecondary = Color(0xFFFFFFFF)
+val HamrahSecondaryContainer = Color(0xFFE0E1F9)
+val HamrahOnSecondaryContainer = Color(0xFF181A2C)
+
+val HamrahTertiary = Color(0xFF77536D)
+val HamrahOnTertiary = Color(0xFFFFFFFF)
+val HamrahTertiaryContainer = Color(0xFFFFD7F0)
+val HamrahOnTertiaryContainer = Color(0xFF2D1228)
+
+val HamrahError = Color(0xFFBA1A1A)
+val HamrahOnError = Color(0xFFFFFFFF)
+val HamrahErrorContainer = Color(0xFFFFDAD6)
+val HamrahOnErrorContainer = Color(0xFF410002)
+
+val HamrahBackground = Color(0xFFFEFBFF)
+val HamrahOnBackground = Color(0xFF1B1B1F)
+val HamrahSurface = Color(0xFFFEFBFF)
+val HamrahOnSurface = Color(0xFF1B1B1F)
+val HamrahSurfaceVariant = Color(0xFFE3E1EC)
+val HamrahOnSurfaceVariant = Color(0xFF46464F)
+val HamrahOutline = Color(0xFF767680)
